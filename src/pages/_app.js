6f536d7e79369e5e6555fbd79f79c8b5da40ffe3
K-Sikora <krzysieksikora00123@gmail.com>
2023-06-05 15:30:31 +0200

    Added title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>just_icons</title>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
